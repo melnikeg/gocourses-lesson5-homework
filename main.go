a432f5e3220702c4933ed79da85d3d02138d9d2f
Egor <melnikeg@ukr.net>
2022-06-16 15:22:26 +0300

    Fixed some bugs

--- main.go
+++ main.go
@@ -4,26 +4,26 @@ import "fmt"
 
 const (
 	priceOfOneAplle float32 = 5.99
-	priceOfOnePear          = 7
-	myCash                  = 23
+	priceOfOnePear  float32 = 7
+	myCash          float32 = 23
 )
 
 func main() {
 
 	var priceOf9Apples = priceOfOneAplle * 9
 	var priceOf8Pears = priceOfOnePear * 8
-	var price9ApplesAnd8Pears = priceOf9Apples + float32(priceOf8Pears)
+	var price9ApplesAnd8Pears = priceOf9Apples + priceOf8Pears
 	fmt.Println("1. Скільки грошей треба витратити, щоб купити 9 яблук та 8 груш?", price9ApplesAnd8Pears)
 
-	var howManyApplesCanBuy = float32(myCash) / priceOfOneAplle
+	var howManyApplesCanBuy = myCash / priceOfOneAplle
 	fmt.Println("2. Скільки груш ми можемо купити?", int(howManyApplesCanBuy))
 
 	var howManyPearsCanBuy = myCash / priceOfOnePear
-	fmt.Println("3. Скільки яблук ми можемо купити?", howManyPearsCanBuy)
+	fmt.Println("3. Скільки яблук ми можемо купити?", int(howManyPearsCanBuy))
 
 	var priceOf2Apples = priceOfOneAplle * 2
 	var priceOf2Pears = priceOf8Pears * 2
-	var priceOf2ApplesAnd2Pears = priceOf2Apples + float32(priceOf2Pears)
+	var priceOf2ApplesAnd2Pears = priceOf2Apples + priceOf2Pears
 	var enoughCashForFruit = myCash > priceOf2ApplesAnd2Pears
 	fmt.Println("4. Чи ми можемо купити 2 груші та 2 яблука?", enoughCashForFruit)
 }
